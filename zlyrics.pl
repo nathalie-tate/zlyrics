3f3d276243fa97c9bdf1ab354e35834a131d7796
Nathalie Tate <nathalie.tate00@gmail.com>
2018-02-25 12:40:05 -0500

    remove unused code

--- zlyrics.pl
+++ zlyrics.pl
@@ -4,18 +4,6 @@ use strict;
 use warnings;
 
 use LWP::Simple;
-use LWP::UserAgent;
-use Data::Dumper;
-
-my $ua = new LWP::UserAgent;
-$ua->agent("Mozilla/8.0");
-
-sub trim
-{
-  my $s = shift;
-  $s =~ s/^\s+|\s+$//g;
-  return $s
-}
 
 my @lyrics;
 
@@ -46,8 +34,6 @@ if ($html =~/1\. <a href="(.+)" target="_blank">/)
 }
 else
 {
-  print Dumper \$html;
-    die;
   die "Lyrics not found.\n";
 }
 
@@ -59,6 +45,5 @@ $output =~ s/<b>//g;
 $output =~ s/<\/b>//g;
 $output =~ s/<br>//g;
 $output =~ s/<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->//;
-
 
 print $output;
